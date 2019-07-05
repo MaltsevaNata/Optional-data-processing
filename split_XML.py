886b3e4b01d0db5edf7ca8d730385fe09225f33c
MaltsevaNata <42980988+MaltsevaNata@users.noreply.github.com>
2019-07-05 11:35:40 +0200

    обрабатывается вся разметка

--- split_XML.py
+++ split_XML.py
@@ -16,6 +16,29 @@ def create_subelement(element, name, text):
     subelement = xml.SubElement(element, name)
     subelement.text = str(text)
 
+def create_object(pictures, class_name):
+    for objects in pictures.iter('ObjectRect'):
+        object = xml.Element("object")
+        create_subelement(object, "name", class_name)
+        create_subelement(object, "pose", "Unspecified")
+        create_subelement(object, "truncated", 0)
+        create_subelement(object, "difficult", 0)
+
+        bndbox = xml.SubElement(object, "bndbox")
+
+        koord = objects.text
+        koord = koord.strip().split()
+        koord = map(int, koord)
+        xmin, ymin, xmax, ymax = bbox_transform_l_corner(koord)
+
+        create_subelement(bndbox, "xmin", int(xmin))
+        create_subelement(bndbox, "ymin", int(ymin))
+        create_subelement(bndbox, "xmax", int(xmax))
+        create_subelement(bndbox, "ymax", int(ymax))
+        root.append(object)
+
+
+exitFlag = False
 old_tree = xml.parse(input_xml)
 old_root = old_tree.getroot()
 for elem in old_root:
@@ -25,71 +48,57 @@ for elem in old_root:
         PictureInfo = subelem.find('PictureInfo')
         for pictures in PictureInfo:
             name = pictures[0].text
-            for file in created_files:
-                if name == file:
-                    #Значит файл уже создан, нужно добавить в него еще объект
-                    tree = xml.parse(name + 'xml')
-                    root = tree.getroot()
-                    break
+            if name == '"01228.jpg"':
+                for file in created_files:
+                    if name == file:
+                        #Значит файл уже создан, нужно добавить в него еще объект
+                        xml_name = name.replace('jpg', 'xml').strip('""')
+                        tree = xml.parse(folder + xml_name)
+                        root = tree.getroot()
+                        create_object(pictures, class_name)
+                        tree.write(folder + xml_name)
+                        exitFlag = True
+                        break
 
-            #создаем новый файл
-            # Каждый блок <_> содержит информацию об отдельном фото
-            #создаем отдельный файл под каждый блок
-            root = xml.Element("annotation")
+                if not exitFlag:
+                    #создаем новый файл
+                    # Каждый блок <_> содержит информацию об отдельном фото
+                    #создаем отдельный файл под каждый блок
+                    root = xml.Element("annotation")
 
-            fold = xml.Element("folder")
-            fold.text = folder
-            root.append(fold)
+                    fold = xml.Element("folder")
+                    fold.text = folder
+                    root.append(fold)
 
-            filename = xml.Element("filename")
-            root.append(filename)
-            filename.text = name
+                    filename = xml.Element("filename")
+                    root.append(filename)
+                    filename.text = name
 
-            path = xml.Element("path")
-            path.text = folder + '/' + filename.text
-            root.append(path)
+                    path = xml.Element("path")
+                    path.text = folder + filename.text
+                    root.append(path)
 
-            source = xml.Element("source")
-            create_subelement(source, "database", "Unknown")
-            root.append(source)
+                    source = xml.Element("source")
+                    create_subelement(source, "database", "Unknown")
+                    root.append(source)
 
-            size = xml.Element("size")
-            create_subelement(size, "width", image_width)
-            create_subelement(size, "height", image_height)
-            create_subelement(size, "depth", image_depth)
-            root.append(size)
+                    size = xml.Element("size")
+                    create_subelement(size, "width", image_width)
+                    create_subelement(size, "height", image_height)
+                    create_subelement(size, "depth", image_depth)
+                    root.append(size)
 
-            segmented = xml.Element("segmented")
-            segmented.text = '0'
-            root.append(segmented)
+                    segmented = xml.Element("segmented")
+                    segmented.text = '0'
+                    root.append(segmented)
 
 
-            try:
-                object = xml.Element("object")
-                create_subelement(object, "name", class_name)
-                create_subelement(object, "pose", "Unspecified")
-                create_subelement(object, "truncated", 0)
-                create_subelement(object, "difficult", 0)
+                    try:
+                        create_object(pictures, class_name)
+                    except:
+                        continue
 
-                bndbox = xml.SubElement(object, "bndbox")
-                for objects in range(len(pictures)):
-                    obj_number = len(pictures[1])
-                    for properties in range(obj_number): #ИСПРАВИТЬ - НУЖНО ШАГАТЬ ПО ОБЪЕКТАМ В КАЖДОМ OBJECTRECTLIST  
-                        if pictures[1][objects][properties].tag == 'ObjectRect':
-                            koord = pictures[1][objects][properties].text
-                            koord = koord.strip().split()
-                            koord = map(int, koord)
-                            xmin, ymin, xmax, ymax = bbox_transform_l_corner(koord)
-
-                            create_subelement(bndbox, "xmin", int(xmin))
-                            create_subelement(bndbox, "ymin", int(ymin))
-                            create_subelement(bndbox, "xmax", int(xmax))
-                            create_subelement(bndbox, "ymax", int(ymax))
-                            root.append(object)
-            except:
-                continue
-
-            # ‭ ‬создаём новый файл XML с одним объектом whitehead
-            tree = xml.ElementTree(root)
-            tree.write(folder + filename.text.replace('jpg', 'xml').strip('""'))
-            created_files.append(filename.text.replace('jpg', '').strip('""'))
+                    # ‭ ‬создаём новый файл XML
+                    tree = xml.ElementTree(root)
+                    tree.write(folder + filename.text.replace('jpg', 'xml').strip('""'))
+                    created_files.append(filename.text)
