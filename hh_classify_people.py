55bbe7fbe2dbea7942a6487430fbb256d7b7cf69
MaltsevaNata <natalia_maltseva@bk.ru>
2019-07-11 14:34:08 +0200

    работает оценка

--- hh_classify_people.py
+++ hh_classify_people.py
@@ -8,41 +8,33 @@ import xml.etree.ElementTree as xml
 import math
 
 
-marked_ann = 'D:/Study/Python/old_hh/check_annot'
-predicted_ann = 'D:/Study/Python/old_hh/predicted_annot'
+marked_ann = open('/home/user/Документы/for_metrics/gt_val.txt', 'r')
+predicted_ann = '/home/user/Документы/for_metrics/hh_vis_det_and_cls_logging'
 
 
 def get_objects(file):
     tree = xml.parse(file)
     root = tree.getroot()
-    #objects = [[None]*3 for i in (range(len(root)-6))] # 0- classname, 1,2 - center coordinates(x,y)
     objects = []
-    #i = 0
     for elem in root:
         list = []
         for subelem in elem:
-
             if subelem.tag == 'name':
                 list.append(subelem.text)
                # objects[i][0] = subelem.text
-
             if subelem.tag == 'bndbox':
                 coords=[]
                 for values in subelem:
                     coords.append(int(values.text))
-                #objects[i][1] = abs(coords[2]-coords[0]) #xmax-xmin
                 list.append(abs(coords[2]-coords[0]))
-                #objects[i][2] = abs(coords[3]-coords[2]) #ymax - ymin
                 list.append(abs(coords[3]-coords[2]))
                 objects.append(list)
-                #i = i + 1
-
     return objects
 
 
 def get_pairs(objects):
     people = []
-    head_types = ['helmet' , 'head', 'hat', 'hood']
+    head_types = ['helmet', 'head', 'hat', 'hood']
     headdress = []
     helmets = []
     for obj in range(len(objects)):
@@ -58,6 +50,8 @@ def get_pairs(objects):
         if objects[obj][0] == 'helmet_off':
             helmets.append(list)
     pairs = []
+    if (len(people) != 0) and (len(headdress) == 0):
+        return pairs, helmets
     for person in people:
         min_dist = 20000
         min_head = ''
@@ -85,45 +79,59 @@ def get_color(person):
             'hood': 'person_orange'
         }[person[0]]
 
-def compare(pairs, predicted_pairs):
+def compare(pairs, predicted_pairs, TP, FP, FN):
     guessed = 0
     not_guessed = len(pairs)
     for pair in pairs:
         for predicted_pair in predicted_pairs:
-            pr_name = predicted_pair[0]
-            name = pair[0]
-            pr_x = predicted_pair[1]
-            pr_y = predicted_pair[2]
-            x = pair[1][0]
-            y = pair[1][1]
             deltax = predicted_pair[1] - pair[1][0]
             deltay = predicted_pair[2] - pair[1][1]
-            if pr_name == name and deltax < 200 and deltay < 200:
-                guessed = guessed + 1
-                not_guessed = not_guessed - 1
-    return guessed, not_guessed
+            if pr_name == name and deltax < 220 and deltay < 220:
+                TP = TP + 1
+            elif pr_name != name and deltax < 220 and deltay < 220:
+                FP = FP + 1
+            else:
+                FN = FN + 1
+    return TP, FP, FN
 
 
-marked = os.listdir(marked_ann)
+marked = marked_ann.readlines()
 predicted = os.listdir(predicted_ann)
 
 marked.sort()
 predicted.sort()
 
+TP = 0  #недалеко и совпал класс
+FP = 0  #класс не совпал
+FN = 0  #ничего не совпало
+fail_num = 0
+
 for ann in marked:
-    with open(marked_ann + '/' + ann, 'r') as f1:
-        box1 = get_objects(f1)
-        pairs, helmets = get_pairs(box1)
-        for person in pairs:
-            person[0] = get_color(person)
-        for helmet in helmets:
-            pairs.append(helmet)
-        f2 = open(predicted_ann + '/' + ann, 'r')
-        predicted_pairs = get_objects(f2)
-        guessed, not_guessed = compare(pairs, predicted_pairs)
-        print('Guessed: ', guessed, end='\n')
-        print('Not guessed: ', not_guessed, end='')
-
-
+    try:
+        i = 0
+        with open(ann[:-1], 'r') as f1:
+            box1 = get_objects(f1)
+            pairs, helmets = get_pairs(box1)
+            for person in pairs:
+                person[0] = get_color(person)
+            for helmet in helmets:
+                pairs.append(helmet)
+            f2 = open(predicted_ann + '/' + 'det_' + str(i) + '.xml', 'r')
+            predicted_pairs = get_objects(f2)
+            TP, FP, FN = compare(pairs, predicted_pairs, TP, FP, FN)
+    except:
+        print('Failed: ', ann, end='\n ')
+        fail_num = fail_num + 1
+        continue
+precision = TP/(TP+FP)
+recall = TP/(TP+FN)
+F1 = 2 * (precision * recall)/(precision + recall)
+print('\n Failed ', fail_num, end=' images \n ')
+print('\n TP: ', TP, end=' \n ')
+print('\n FP: ', FP, end=' \n ')
+print('\n FN: ', FN, end=' \n ')
+print('\n precision: ', precision, end=' \n ')
+print('\n recall: ', recall, end=' \n ')
+print('\n F1: ', F1, end=' \n ')
 
 
